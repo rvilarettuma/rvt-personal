c2212669b26def91efa2e6dbb0191487125ac5a0
rvilarettuma <rvilaret@colostate.edu>
2020-11-19 17:05:23 -0700

    Add bio lorem ipsum

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './fonts/futura-bold.ttf';
-import './fonts/futura-medium.woff'
+import './fonts/futura-medium.woff';
+import './fonts/Futura-Book-BT.woff';
 
 ReactDOM.render(
   <React.StrictMode>
