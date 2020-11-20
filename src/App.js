6f1d87fc566615c02e940c54ad605dafd8c3e40d
rvilarettuma <rvilaret@colostate.edu>
2020-11-20 16:02:28 -0700

    Add CNAME file

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { Container } from "@material-ui/core";
 function App() {
   return (
     <div className="App">
-      <Container>
+      <Container maxWidth="lg">
+      
         <header className="App-header">
+          
           <h1>Richard Vilaret-Tuma</h1>
           <h2>Student IT Administrator</h2>
-          
         </header>
+        
         <body>
-          <h3>Biography</h3>
+          <h4>BIOGRAPHY</h4>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
