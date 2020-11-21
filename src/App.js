114250f03926896e5d8c9833002d79252009860b
rvilarettuma <rvilaret@colostate.edu>
2020-11-20 18:56:20 -0700

    Add bio

--- src/App.js
+++ src/App.js
@@ -5,24 +5,56 @@ function App() {
   return (
     <div className="App">
       <Container maxWidth="lg">
-      
         <header className="App-header">
-          
           <h1>Richard Vilaret-Tuma</h1>
-          <h2>Student IT Administrator</h2>
+          <h2>Adminstrator</h2>
         </header>
-        
+
         <body>
-          <h4>BIOGRAPHY</h4>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+          <div className="subheader">
+            <h4>Biography</h4>
+            <hr />
+          </div>
+          <div className="bio">
+            <p>
+              The origin of my relationship with computers: 
+              <em>The Computer Room</em>. A vestige of the early 2000's; The
+              dedicated room for a beige Compaq and it's shrieking counterpart:
+              the modem. The nightly ritual of tolerating dial-up screeching and
+              launching up RuneScape was enough to enthrall me. It was also
+              enough for my immigrant parents to brand me as the resident
+              computer expert, a role I accepted with great reverance.
+            </p>
+            <p>
+              I was fortunate enough to come across a group of friends who also
+              shared my enjoyment of computer games. Through them, and with some
+              help from my grandfather, I hand selected components and built my
+              first gaming PC. This was quite the endeavor, but the experience
+              granted me invaluable hardware knowledge and troubleshooting
+              skills.
+            </p>
+            <p>
+              By high school, I had managed to establish quite the technological
+              foundation for myself. I joined the school's PC Support group,
+              where I actively served as a technician for the school's 4000
+              students and accompanying faculty. I partook in an exclusive
+              software engineering mentorship program with the Miami-Dade County
+              IT Department. I earned several Microsoft and Adobe
+              certifications. I even managed to win Miami-Dade County Public
+              Schools Hour of Code competion, where my entry was selected (in
+              the winning Top 20) from a pool of 530 contenders.
+            </p>
+            <p>
+              Presently, I am employed at Colorado State University as a Student
+              IT Administrator. Since I have completed my Bachelor of Science in
+              Applied Computing Technology, I am currently looking for work
+              across a variety of fields. System Administration, DevOps, and
+              Web/Software Development are all fields I've dabbled in, and would
+              love to purse in the future.
+            </p>
+          </div>
+
+          <h4>PROJECTS</h4>
         </body>
       </Container>
     </div>
