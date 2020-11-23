b25b8a107bc923f09c746332adab52b1ebbf6766
rvilarettuma <rvilaret@colostate.edu>
2020-11-23 16:07:01 -0700

    Add h1 typography theme

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import "./App.css";
-import { Container } from "@material-ui/core";
+import { Container, Typography } from "@material-ui/core";
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
 import Bio from "./Application/Bio";
 import Projects from "./Application/Projects";
 
+const theme = createMuiTheme({
+  typography: {
+    h1: {
+      whiteSpace: 'pre-line',
+      fontFamily: "futura-bold",
+      textTransform: 'uppercase',
+      letterSpacing: '0.5rem',
+      fontSize: '3.125em',
+      color: '#0D0D0D',
+    },
+    h2: {
+      fontFamily: "futura-medium",
+      textTransform: 'lowercase',
+      letterSpacing: '0.2rem',
+      fontSize: '2.375em',
+      color: '#0D0D0D',
+    }
+  }
+});
+
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
       <Container maxWidth="lg">
         <header className="App-header">
-          <h1>Richard Vilaret-Tuma</h1>
+          <Typography variant="h1">Richard Vilaret-Tuma</Typography>
           <h2>SysAdmin, Developer, DevOps</h2>
         </header>
-        <body>
-          <Bio />
-          <Projects />
-        </body>
+        <Bio />
+        <Projects />
       </Container>
     </div>
+    </ThemeProvider>
   );
 }
 
