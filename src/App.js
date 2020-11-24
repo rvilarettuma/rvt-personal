6a92c60b021a6302fceb69e4bbb5ad2a723a03a9
rvilarettuma <rvilaret@colostate.edu>
2020-11-23 17:16:56 -0700

    Adjust color 2: Electric Boogaloo

--- src/App.js
+++ src/App.js
@@ -19,6 +19,9 @@ const theme = createMuiTheme({
     },
     text: {
       primary: 'rgba(13,13,13,0.87)',
+      secondary: 'rgba(13,13,13,0.54)',
+      disabled: 'rgba(13,13,13,0.38)',
+      hint: 'rgba(13,13,13,0.38)',
     }
   },
   typography: {
@@ -28,32 +31,38 @@ const theme = createMuiTheme({
       textTransform: 'uppercase',
       letterSpacing: '0.5rem',
       fontSize: '3.125em',
+      color: 'rgba(13,13,13)',
     },
     h2: {
       fontFamily: "futura-medium",
       textTransform: 'lowercase',
       letterSpacing: '0.2rem',
       fontSize: '2.375em',
+      color: 'rgba(13,13,13)',
     },
     h3: {
       fontFamily: "futura-bold",
       letterSpacing: '0.5',
       fontSize: '1.75em',
+      color: 'rgba(13,13,13)',
     },
     h4: {
       fontFamily: "futura-bold",
       letterSpacing: '0.5',
       fontSize: '1.3125em',
+      color: 'rgba(13,13,13)',
     },
     body1: {
       fontFamily: "futura-book",
       fontSize: '1.125em',
       lineHeight: '1.6875rem',
+      color: 'rgba(13,13,13)',
     },
     subtitle1: {
       fontFamily: "futura-book",
       fontSize: '1.125em',
       lineHeight: '1.6875rem',
+      color: 'rgba(13,13,13)',
     }
   }
 });
@@ -64,8 +73,8 @@ function App() {
     <div className="App">
       <Container maxWidth="lg">
         <header className="App-header">
-          <Typography variant="h1">Richard Vilaret-Tuma</Typography>
-          <Typography variant="h2">SysAdmin, Developer, DevOps</Typography>
+          <Typography component="h1" variant="h1">Richard Vilaret-Tuma</Typography>
+          <Typography component="h2" variant="h2">SysAdmin, Developer, DevOps</Typography>
         </header>
         <Bio />
         <Projects />
