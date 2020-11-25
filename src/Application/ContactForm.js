d5325a4de3747ea6316bfbeb4ef7e40d0f6b0e1c
rvilarettuma <rvilaret@colostate.edu>
2020-11-25 12:04:08 -0700

    Add formspree

--- src/Application/ContactForm.js
+++ src/Application/ContactForm.js
@@ -4,7 +4,6 @@ import Button from "@material-ui/core/Button";
 import Send from "@material-ui/icons/Send";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
-import axios from "axios";
 
 class ContactForm extends Component {
   constructor(props) {
