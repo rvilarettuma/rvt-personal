927b8492241d2c896d01e474413d44d1d3f2fbb2
rvilarettuma <rvtuma.dev@gmail.com>
2022-01-23 12:52:21 -0700

    Add MUI UI framework

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import About from './Components/About/About';
 
 function App() {
   return (
     <div className="App">
-      <header>
+      <header className="App-header">
         <h1>Richard Vilaret-Tuma</h1>
       </header>
+      <About />
     </div>
   );
 }
--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import text from './Text.json'
 
 function About() {
   return (
     <div>
-      
+      <h2>About</h2>
+      <p>{text.about}</p>
     </div>
   );
 }
