5471be7f517b39f2c238985f0d5a6c27003af0a3
rvilarettuma <rvtuma.dev@gmail.com>
2022-01-23 11:40:11 -0700

    Rebuild base create-react-app project structure

--- src/App.js
+++ src/App.js
@@ -1,110 +1,24 @@
-import "./App.css";
-import Container from "@material-ui/core/Container"
-import Typography from "@material-ui/core/Typography";
-import { createMuiTheme } from '@material-ui/core/styles';
-import { ThemeProvider } from '@material-ui/styles';
-
-import Bio from "./Application/Bio";
-import Projects from "./Application/Projects";
-import ContactForm from "./Application/ContactForm";
-import Footer from "./Application/Footer";
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#FAF6F3',
-      contrastText: 'rgba(13,13,13,0.87)'
-    },
-    secondary: {
-      main: '#516652',
-    },
-    background: {
-      main: '#FAF6F3',
-    },
-    text: {
-      primary: 'rgba(13,13,13,0.87)',
-      secondary: 'rgba(13,13,13,0.54)',
-      disabled: 'rgba(13,13,13,0.38)',
-      hint: 'rgba(13,13,13,0.38)',
-    }
-  },
-  typography: {
-    h1: {
-      whiteSpace: 'pre-line',
-      fontFamily: "futura-bold",
-      textTransform: 'uppercase',
-      letterSpacing: '0.5rem',
-      fontSize: '3.125em',
-      color: 'rgba(13,13,13)',
-      '@media screen and (max-width: 820px)': {
-        fontSize: '2.375em',
-      }
-    },
-    h2: {
-      fontFamily: "futura-medium",
-      textTransform: 'lowercase',
-      letterSpacing: '0.2rem',
-      fontSize: '2.375em',
-      color: 'rgba(13,13,13)',
-      '@media screen and (max-width: 820px)': {
-        fontSize: '1.75em',
-      }
-    },
-    h3: {
-      fontFamily: "futura-bold",
-      letterSpacing: '0.5',
-      fontSize: '1.75em',
-      color: 'rgba(13,13,13)',
-      '@media screen and (max-width: 820px)': {
-        fontSize: '1.3125em',
-      }
-    },
-    h4: {
-      fontFamily: "futura-bold",
-      letterSpacing: '0.5',
-      fontSize: '1.3125em',
-      color: 'rgba(13,13,13)',
-      '@media screen and (max-width: 820px)': {
-        fontSize: '1em',
-      }
-    },
-    body1: {
-      fontFamily: "futura-book",
-      fontSize: '1.125em',
-      lineHeight: '1.6875rem',
-      color: 'rgba(13,13,13)',
-      '@media screen and (max-width: 820px)': {
-        fontSize: '1em',
-      }
-    },
-    subtitle1: {
-      fontFamily: "futura-book",
-      fontSize: '1.125em',
-      lineHeight: '1.6875rem',
-      color: 'rgba(13,13,13,0.84)',
-      '@media screen and (max-width: 820px)': {
-        fontSize: '1em',
-      }
-    }
-  }
-});
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
     <div className="App">
-      <Container maxWidth="lg">
-        <header className="App-header">
-          <Typography component="h1" variant="h1">Richard Vilaret-Tuma</Typography>
-          <Typography component="h2" variant="h2">SysAdmin, Developer, DevOps</Typography>
-        </header>
-        <Bio />
-        <Projects />
-        <ContactForm />
-        <Footer />
-      </Container>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-    </ThemeProvider>
   );
 }
 
