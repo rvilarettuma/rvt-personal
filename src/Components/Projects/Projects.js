b9f70dbbbfcac469a0b4576ad1a612a5e719f53b
rvilarettuma <rvtuma.dev@gmail.com>
2022-01-23 22:06:15 -0700

    Add Projects Accordion

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,12 +1,105 @@
-import React from 'react';
-import { Typography } from '@mui/material';
-import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
+import React from "react";
+import { Typography } from "@mui/material";
+import { Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import CodeIcon from "@mui/icons-material/Code";
+import LinkIcon from "@mui/icons-material/Link";
+import projects from "./Projects.json";
+
+function getProjects() {
+  var projectArray = projects;
+  var result = [];
+
+  for (const project in projectArray) {
+    var name = projectArray[project].name;
+    var stack = projectArray[project].stack;
+    var description = projectArray[project].description;
+    var link = projectArray[project].link;
+    var source = projectArray[project].source;
+    result.push(
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Stack>
+            <Typography>
+              <strong>{name}</strong>
+            </Typography>
+            <Typography sx={{ color: "text.secondary" }}>{stack}</Typography>
+          </Stack>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>{description}</Typography>
+          <br/>
+          <Stack direction="row" spacing={2}>
+            {link ? (
+              <Button variant="contained" href={link} startIcon={<LinkIcon />}>
+                Project
+              </Button>
+            ) : null}
+            {source ? (
+              <Button
+                variant="contained"
+                href={source}
+                startIcon={<CodeIcon />}
+              >
+                Source
+              </Button>
+            ) : null}
+          </Stack>
+        </AccordionDetails>
+      </Accordion>
+    );
+  }
+
+  return result;
+}
 
 function Projects() {
-  return(
+  return (
     <div>
-      <Typography component="h2" variant="h4">Projects</Typography>
-
+      <Typography component="h2" variant="h4">
+        Projects
+      </Typography>
+      {getProjects()}
+      {/* <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header">
+          <Typography>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography>Disabled Accordion</Typography>
+        </AccordionSummary>
+      </Accordion> */}
     </div>
   );
 }
